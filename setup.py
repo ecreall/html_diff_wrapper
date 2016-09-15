2dd0da099f45819ae56da6f37c3b05f6e5b82e9d
Vincent Fretin <vincent.fretin@gmail.com>
2016-09-15 16:48:31 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ requires = [
 
 setup(
     name='html_diff_wrapper',
-    version='1.0',
+    version='1.0.1.dev0',
     description='A html diff wrapper',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
